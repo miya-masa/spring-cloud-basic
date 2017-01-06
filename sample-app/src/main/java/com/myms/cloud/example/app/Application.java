633316328f413c06e91566731fe2f09644692885
masayuki-miyauchi <miyauchi0623@gmail.com>
2017-01-07 01:14:54 +0900

    apply eureka client

--- sample-app/src/main/java/com/myms/cloud/example/app/Application.java
+++ sample-app/src/main/java/com/myms/cloud/example/app/Application.java
@@ -1,25 +1,36 @@
 package com.myms.cloud.example.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
 public class Application {
 
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(Application.class, args);
-  }
+    /** ロガー */
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-  @Value("${info.foo}")
-  private String hello;
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 
-  @RequestMapping(value = "hello", method = RequestMethod.GET)
-  public String hello() {
-    return hello;
-  }
+    @Value("${info.foo}")
+    private String hello;
+
+    @Value("${hello.sample}")
+    private String sample;
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    public String hello() {
+        log.info(sample);
+        return hello;
+    }
 }
