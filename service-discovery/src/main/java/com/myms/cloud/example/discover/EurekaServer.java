6ca5da47581d36149f9dd8d46d842ec9e47589a8
masayuki-miyauchi <miyauchi0623@gmail.com>
2017-01-06 20:52:56 +0900

    enable Eureka Server

---
+++ service-discovery/src/main/java/com/myms/cloud/example/discover/EurekaServer.java
@@ -0,0 +1,15 @@
+package com.myms.cloud.example.discover;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+    public static void main(String[] args) throws Exception {
+        new SpringApplicationBuilder(EurekaServer.class).web(true).run(args);
+    }
+
+}
