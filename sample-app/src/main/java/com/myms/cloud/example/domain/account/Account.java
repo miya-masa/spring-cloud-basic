d92ab8b62a9db6bad7e1c5474ff584af50738416
masayuki-miyauchi <miyauchi0623@gmail.com>
2017-01-11 21:55:40 +0900

    add @Data annotation

--- sample-app/src/main/java/com/myms/cloud/example/domain/account/Account.java
+++ sample-app/src/main/java/com/myms/cloud/example/domain/account/Account.java
@@ -1,5 +1,14 @@
 package com.myms.cloud.example.domain.account;
 
+import lombok.Data;
+
+@Data
 public class Account {
+
+    private long userId;
+
+    private String email;
+
+    private String password;
 
 }
