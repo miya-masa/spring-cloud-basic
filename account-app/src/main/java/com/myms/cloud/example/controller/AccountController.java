838acc9118582cdb7cabb8600f5b0c8c817389a6
masayuki-miyauchi <miyauchi0623@gmail.com>
2017-01-21 21:41:06 +0900

    WIP(change directory structure)

--- account-app/src/main/java/com/myms/cloud/example/controller/AccountController.java
+++ account-app/src/main/java/com/myms/cloud/example/controller/AccountController.java
@@ -2,10 +2,13 @@ package com.myms.cloud.example.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.myms.cloud.example.application.AccountUsecase;
 
 @RestController
 public class AccountController {
@@ -13,15 +16,7 @@ public class AccountController {
     /** ロガー */
     private static final Logger log = LoggerFactory.getLogger(AccountController.class);
 
-    @Value("${info.foo}")
-    private String hello;
+    @Autowired
+    private AccountUsecase accountUsecase;
 
-    @Value("${hello.sample}")
-    private String sample;
-
-    @RequestMapping(value = "hello", method = RequestMethod.GET)
-    public String hello() {
-        log.info(sample);
-        return hello;
-    }
 }
---
+++ account-app/src/main/java/com/myms/cloud/example/domain/account/Login.java
@@ -0,0 +1,9 @@
+package com.myms.cloud.example.domain.account;
+
+public class Login {
+
+    private String username;
+
+    private String password;
+
+}
