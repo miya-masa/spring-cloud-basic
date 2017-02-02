7517c034a46821e6c259693e9f1af4ec54f926ee
masayuki-miyauchi <miyauchi0623@gmail.com>
2017-02-02 22:34:50 +0900

    add spring security oAuth

--- account-app/src/main/java/com/myms/cloud/example/Application.java
+++ account-app/src/main/java/com/myms/cloud/example/Application.java
@@ -5,9 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableResourceServer
 public class Application {
 
     /** ロガー */
