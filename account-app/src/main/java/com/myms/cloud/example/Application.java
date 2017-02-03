9d3146b94a326bfd52c97b63b7e7847eeffba269
masayuki-miyauchi <miyauchi0623@gmail.com>
2017-02-03 23:47:11 +0900

    extends ResourceServerConfigurerAdapter

--- account-app/src/main/java/com/myms/cloud/example/Application.java
+++ account-app/src/main/java/com/myms/cloud/example/Application.java
@@ -5,12 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableResourceServer
-public class Application {
+public class Application extends ResourceServerConfigurerAdapter {
 
     /** ロガー */
     private static final Logger log = LoggerFactory.getLogger(Application.class);
@@ -18,5 +21,13 @@ public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {}
 
 }
